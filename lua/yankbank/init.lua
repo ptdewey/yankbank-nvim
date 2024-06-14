16e6d7efd3f1cecb471dde5d4fed0f21f0ffb648
ptdewey <patrickdewey2002@gmail.com>
2024-06-14 13:24:50 -0400

    fix: fixed error on yank/delete with no specified opts

--- lua/yankbank/init.lua
+++ lua/yankbank/init.lua
@@ -8,35 +8,39 @@ local clipboard = require("yankbank.clipboard")
 -- initialize yanks tables
 local yanks = {}
 local reg_types = {}
-local max_entries = 10
-local sep = "-----"
+local default_opts = {
+    max_entries = 10,
+    sep = "-----",
+    focus_gain_poll = false,
+    num_behavior = "prefix",
+    registers = {
+        yank_register = "+",
+    },
+    keymaps = { },
+}
 
 -- wrapper function for main plugin functionality
 local function show_yank_bank(opts)
     -- Parse command arguments directly if args are provided as a string
-    opts = opts or {}
+    opts = opts or default_opts
 
-    -- Fallback to defaults if necessary
-    local max_entries_opt = opts.max_entries or max_entries
-    local sep_opt = opts.sep or sep
-    opts.keymaps = opts.keymaps or {}
-
+    -- initialize buffer and populate bank
     local bufnr, display_lines, line_yank_map =
-        menu.create_and_fill_buffer(yanks, reg_types, max_entries_opt, sep_opt)
+        menu.create_and_fill_buffer(yanks, reg_types, opts.max_entries, opts.sep)
+
     -- handle empty bank case
     if not bufnr then
         return
     end
+
+    -- open window and set keybinds
     local win_id = menu.open_window(bufnr, display_lines)
     menu.set_keymaps(win_id, bufnr, yanks, reg_types, line_yank_map, opts)
 end
 
 -- plugin setup
 function M.setup(opts)
-    opts = opts or {}
-
-    -- parse opts
-    max_entries = opts.max_entries or max_entries
+    opts = opts or default_opts
 
     -- create clipboard autocmds
     clipboard.setup_yank_autocmd(yanks, reg_types, opts)
